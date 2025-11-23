12b0dfa4b050e1a04311d8ce78b552757c8fa2b5
kushalvk <kushalvaghela2003@gmail.com>
2025-11-23 18:16:55 +0530

    First Commit

---
+++ First/GettingStarted_01.py
@@ -0,0 +1,79 @@
+# All opration are perform in IDLE Shell
+
+
+# 4 + 3
+# 7
+
+# 10 - 3
+# 7
+
+# 10 * 3
+# 30
+
+# 10 / 5
+# 2.0
+
+# 10 // 5
+# 2
+
+# 5/2
+# 2.5
+
+# 5 // 2
+# 2
+
+# 8 + 9 - 10
+# 7
+
+# 8 + 2 * 3
+# 14
+
+# (8 + 2) * 3
+# 30
+
+# 2 * 2 * 2
+# 8
+
+# 2 ** 3
+# 8
+
+# 10 // 3
+# 3
+
+# 10 % 3
+# 1
+
+# 'Kushal'
+# 'Kushal'
+
+# print('Kushal')
+# Kushal
+
+# print('Kushal's laptop')
+# SyntaxError: unterminated string literal (detected at line 1)
+
+# print("Kushal's laptop")
+# Kushal's laptop
+
+# print('Kushal "laptop"')
+# Kushal "laptop"
+
+# print('Kushal's "laptop"')
+# SyntaxError: unterminated string literal (detected at line 1)
+
+# print('Kushal\'s "laptop"') -> ignore that specific meaning in python for that cararecter
+# Kushal's "laptop"
+
+# 'Kushal' + 'Kushal'
+# 'KushalKushal'
+
+# 7 * ' Kushal'
+# ' Kushal Kushal Kushal Kushal Kushal Kushal Kushal'
+
+# print('c:\docs\navin')
+# c:\docs
+# avin
+
+# print(r'c:\docs\navin') -> (r) means row string , that not convert all in a special meaning
+# c:\docs\navin
+
